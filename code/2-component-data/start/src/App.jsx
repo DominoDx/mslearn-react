41f7126843501eae59d92c02347472620a31ad40
dominodx <domino.daoxuan@gmail.com>
2021-12-04 09:38:03 +0700

    props and ternary operator

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -6,12 +6,25 @@ import RecipeTitle from './RecipeTitle'
 
 function App() {
     // TODO: Add recipe object
-
+    const recipe = {
+        title: 'Mashed potatoes',
+        feedback: {
+            rating: 3,
+            reviews: 20
+        },
+        ingredients: [
+            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+            { name: '4 Tbsp butter', prepared: false },
+            { name: '1/8 cup heavy cream', prepared: false },
+            { name: 'Salt', prepared: false },
+            { name: 'Pepper', prepared: false },
+        ],
+    };
     return (
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-
+            <RecipeTitle title={ recipe.title} feedback= { recipe.feedback } />
             {/* TODO: Add IngredientList component */}
 
         </article>
